e7a23645b99aac02aa361ffbdd8be05411b7812b
IoYamada <e1q19092@st.oit.ac.jp>
2022-06-27 10:32:53 +0900

    add gradle progect

---
+++ .log/.history/sequality/app/src/test/java/sequality/CalculateTest_20220627102625.java
@@ -0,0 +1,11 @@
+package sequality;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+  @Test
+  public void testSum() {
+    Calculate calculate = new Calculate();
+  }
+}
